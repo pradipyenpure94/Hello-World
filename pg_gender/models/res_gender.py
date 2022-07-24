acf89c1a6c149ecab8ada89e34e925f565c5488b
prye-odoo <prye@odoo.com>
2022-07-24 15:57:02 +0530

    [ADD] added new field

--- pg_gender/models/res_gender.py
+++ pg_gender/models/res_gender.py
@@ -4,6 +4,6 @@ class ResGender(models.Model):
     _name = 'res.gender'
     _description = 'Gender'
 
-    # name = fields.Char(string='Name',required=True)
+    name = fields.Char(string='Name',required=True)
     f_name = fields.Char(string='First Name',required=True)
     personal_num = fields.float(" pernoal loan")
