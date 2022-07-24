f7bb367d5dd46761ec91ca8b6ab521341ceda0d0
prye-odoo <prye@odoo.com>
2022-07-24 12:39:12 +0530

    [IMP] devloped a unlink function

--- firebits_res_creation/models/models.py
+++ firebits_res_creation/models/models.py
@@ -17,12 +17,12 @@ class ResourcesAccessMixin(models.AbstractModel):
 
         return super().create(vals)
 
-    # def unlink(self):
-    #     """test"""
-    #     ###added a validations
-    #     if self.env.user:
-    #         if not self.env.user.has_group('firebits_res_creation.group_create_res'):
-    #             raise AccessError(hq_warnning)
+    def unlink(self):
+        """test"""
+        ###added a validations
+        if self.env.user:
+            if not self.env.user.has_group('firebits_res_creation.group_create_res'):
+                raise AccessError(hq_warnning)
 
     #     return super().unlink()
 
