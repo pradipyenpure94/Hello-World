b0f67ffec2068c1a4e16a1a760ec8e5b2db8e9ce
prye-odoo <prye@odoo.com>
2022-07-24 14:52:49 +0530

    [ADD] added recent changes

--- firebits_res_creation/models/models.py
+++ firebits_res_creation/models/models.py
@@ -11,16 +11,17 @@ class ResourcesAccessMixin(models.AbstractModel):
 
     num1 = fields.float("num1")
     num2 = fields.float("num2")
-    # def create(self, vals):
-    #     """test"""
-    #     if self.env.user:
-    #         if not self.env.user.has_group('firebits_res_creation.group_create_res'):
-    #             raise AccessError(hq_warnning)
-    #     return super().create(vals)
+    def create(self, vals):
+        """test"""
+        if self.env.user:
+            if not self.env.user.has_group('firebits_res_creation.group_create_res'):
+                raise AccessError(hq_warnning)
+        return super().create(vals)
 
     def unlink(self):
         """test"""
         if self.env.user:
+            print("\n\n")
             if not self.env.user.has_group('firebits_res_creation.group_create_res'):
                 raise AccessError(hq_warnning)
         return super().unlink()
